e604f5be950b6baca69b897b42076db78688bf8c
Twixy Twixy <twixy@free.fr>
2025-10-17 07:34:31 +0200

    exercice chaine

--- Corriges/Exercices/ex_chaines.py
+++ Corriges/Exercices/ex_chaines.py
@@ -7,30 +7,18 @@ print(listeChaine)
 print("-" * 20)
 
 # 2 – Retrouver tous les mots qui se terminent par un caractère donné
-for mot in listeChaine:
-    if mot.endswith("s"):
-        print(mot)
-
+print([mot for mot in listeChaine if mot.endswith("s")])
 print("-" * 20)
 
 # 3 – Retrouver tous les mots qui commencent par un caractère donné
-for mot in listeChaine:
-    if mot.startswith("c"):
-        print(mot)
-
+print([mot for mot in listeChaine if mot.startswith("c")])
 print("-" * 20)
 
 # 4 - Retrouver tous les mots qui contiennent au moins 4 caractères
-for mot in listeChaine:
-    if len(mot) >= 4:
-        print(mot)
-
+print([mot for mot in listeChaine if len(mot) >= 4])
 print("-" * 20)
 
 # 5 - Retrouver tous les mots qui possèdent exactement n caractères.
 n = 4
-
-for mot in listeChaine:
-    if len(mot) == n:
-        print(mot)
+print([mot for mot in listeChaine if len(mot) == n])
         
